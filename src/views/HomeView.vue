fcaed1f4c2875e001ea945c3389a7431efa649f6
a1 <albert.qwerty.fint@mail.ru>
2025-04-01 19:10:25 +0300

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1 class="uppercase font-bold underline font-roboto">Hello world</h1>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "base-primary":"#00668a",
+        "base-secondary":"#004e71"
+      }
+    },
+    fontFamily: {
+      Roboto: ['roboto, sans-serif']
+    },
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
+  },
+  plugins: [],
+}
+
